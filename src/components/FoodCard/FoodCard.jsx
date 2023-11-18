3da0cb6dd401a65df3ef0758117127641098e84c
mrana007 <rana.es.bc@gmail.com>
2023-11-19 00:32:46 +0600

    implement menu data from mongodb

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -7,11 +7,11 @@ const FoodCard = ({item}) => {
           <img src={image} alt="Order Food" />
         </figure>
         <p className="absolute right-0 mr-4 mt-4 px-6 py-3 bg-black text-white">${price}</p>
-        <div className="card-body items-center text-center">
+        <div className="card-body items-center">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-neutral bg-[#E8E8E8] text-[#BB8506] uppercase">Add to Cart</button>
+            <button className="btn btn-outline border-0 border-b-4 bg-[#E8E8E8] text-[#BB8506] uppercase">Add to Cart</button>
           </div>
         </div>
       </div>
