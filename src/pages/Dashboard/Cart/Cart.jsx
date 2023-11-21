5d25939498210f086d6370a8459d51b9e8ed55fd
mrana007 <rana.es.bc@gmail.com>
2023-11-22 03:01:17 +0600

    implement delete functionality to delete item carts from my cart

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,38 @@
 import { FaTrashAlt } from "react-icons/fa";
 import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-  const [cart] = useCart();
-  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const [cart, refetch] = useCart();
+  const totalPrice = parseFloat(cart.reduce((total, item) => total + item.price, 0)).toFixed(2);
+  const axiosSecure = useAxiosSecure();
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div>
       <div className=" flex flex-col md:flex-row justify-between mb-9">
@@ -41,7 +70,12 @@ const Cart = () => {
                 <td>{item.name}</td>
                 <td>${item.price}</td>
                 <th>
-                  <button className="btn btn-ghost btn-lg"><FaTrashAlt className="text-red-500"></FaTrashAlt></button>
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-500"></FaTrashAlt>
+                  </button>
                 </th>
               </tr>
             ))}
